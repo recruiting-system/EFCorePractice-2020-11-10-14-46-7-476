df6b9510317fe478b04d8429c7232ac42b0303c3
linwenjun <linwenjun@163.com>
2020-11-10 14:46:20 +0000

    first commit

---
+++ EFCorePractice/Controllers/CompanyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EFCorePractice.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EFCorePractice.Controllers
+{
+    [ApiController]
+    [Route("companies")]
+    public class CompanyController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Company>> List()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<Company> Add(Company company)
+        {
+            return null;
+        }
+    }
+}
---
+++ EFCorePractice/Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace EFCorePractice.Models
+{
+    public class Company
+    {
+        public Company()
+        {
+        }
+
+        public Company(string name)
+        {
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
